0ac46e53b81e3e3b0ae5b28eaebaa4833bb2e5af
tengbiao <503354555@qq.com>
2017-10-27 21:05:18 +0800

    1.项目代码初始页面

---
+++ Code/LiBiaoStore/Controllers/LibiaoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiBiaoStore.Web.Controllers
+{
+    /// <summary>
+    /// 网站Controller
+    /// </summary>
+    public class LibiaoController : Controller
+    {
+        // GET: Libiao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Code/LiBiaoStore/Controllers/WechatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiBiaoStore.Web.Controllers
+{
+    /// <summary>
+    /// 微信公众号对接接口
+    /// </summary>
+    public class WechatController : Controller
+    {
+        // GET: Wechat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
